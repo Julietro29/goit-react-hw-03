fc6512339a4da4722ac4718f360e511d29847527
Julietro29 <mousemiu1@gmail.com>
2024-03-25 02:14:22 +0200

    Add all tasks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,62 @@
-import userData from '../userData.json';
-import friends from '../friends.json';
-import transactions from '../transactions.json';
+import { useState, useEffect } from 'react';
+import { nanoid } from 'nanoid';
+import { Title } from './Title/Title';
+import { ContactsList } from './ContactList/ContactsList';
+import { SearchBox } from './SearchBox/SearchBox';
+import { ContactForm } from './ContactForm/ContactForm';
 
-import { Profile } from './Profile/Profile';
-import { FriendList } from './Friends/FriendList';
-import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import { getContactsList } from '../auxiliary/localstorage/getContactsList';
+import { saveContactsList } from '../auxiliary/localstorage/saveContactsList';
 
-export default function App() {
-  const { username, tag, location, avatar, stats } = userData;
+import { SEARCH_LABEL, TITLE } from '../auxiliary/constants';
+
+import styles from './App.module.css';
+
+const App = () => {
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = getContactsList();
+    return savedContacts ? savedContacts : [];
+  });
+  const [filter, setFilter] = useState('');
+
+  const handleAddContact = newContact => {
+    setContacts(prevContacts => [
+      ...prevContacts,
+      { ...newContact, id: nanoid() },
+    ]);
+  };
+
+  const handleDeleteContact = id => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== id)
+    );
+  };
+
+  const handleChangeSearch = event => {
+    setFilter(event.target.value);
+  };
+
+  useEffect(() => {
+    saveContactsList(contacts);
+  }, [contacts]);
+
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
-    <div className="container">
-      <Profile
-        name={username}
-        tag={tag}
-        location={location}
-        image={avatar}
-        stats={stats}
+    <div className={styles.container}>
+      <Title>{TITLE}</Title>
+      <ContactForm onAddContact={handleAddContact} />
+      <SearchBox value={filter} onChange={handleChangeSearch}>
+        {SEARCH_LABEL}
+      </SearchBox>
+      <ContactsList
+        contacts={filteredContacts}
+        onDeleteContact={handleDeleteContact}
       />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
     </div>
   );
-}
+};
+
+export default App
